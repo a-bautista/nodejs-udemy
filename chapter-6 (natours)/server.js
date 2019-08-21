bc64c14de1eee41e624c801c4c92b4ca842dcbb0
Alejandro Bautista <abautista@psi.network>
2019-08-20 22:27:19 -0600

    Connected the app with mongodb

--- chapter-6 (natours)/server.js
+++ chapter-6 (natours)/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' }); // read the variables from the config.env
 const app = require('./app');
@@ -5,7 +6,20 @@ const app = require('./app');
 // see your dev variables
 // console.log(app.get('env'));
 
-console.log(process.env);
+//console.log(process.env);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    userCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(connection => {
+    console.log(connection.connections);
+    console.log('DB connections successful!');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
